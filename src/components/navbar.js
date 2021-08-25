38f37290b65529b1d8bf77aea290a5b2656b6fe8
HugoMoran3 <hugo.moran3@gmail.com>
2021-08-25 15:24:43 +0930

    Changes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { AnchorLink } from "gatsby-plugin-anchor-links"
 import Logo from "../images/portfolio-logo.svg"
 
 const Navbar = () => {
@@ -21,26 +20,30 @@ const Navbar = () => {
                 
                 <li className="navbar-element">
                     <Link
-                    to="/"
+                    to="#about"
                     className="navbar-link"
                     >About</Link>
                 </li>
                 <li className="navbar-element">
                     <Link
-                    to="/"
+                    to="#projects"
                     className="navbar-link"
                     >Projects</Link >
                 </li>
                 <li className="navbar-element">
                     <Link
-                    to="/"
+                    to="#contact"
                     className="navbar-link"
                     >Contact</Link>
                 </li>
                 <li className="navbar-element">
-                    <Link
-                    to="/"
-                    >Resume</Link>
+
+                    <a
+                    target="_blank"
+                    className="navbar-link"
+                    href="https://docs.google.com/document/d/1Qxo-Dd27Uix5kplz78sdbmHCLk-MqoESMMY_QvXFvYQ/edit?usp=sharing">
+                        Resume
+                    </a>
                 </li>
             </ul>
         </div>
--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -3,7 +3,7 @@ import * as React from "react"
 
 const About = () => {
     return(
-        <div className="about">
+        <div className="about" id="about">
             <h1>About me </h1>
             <p>Some of the technologies that I have experience with include: 
             </p>

2e9670b0d8851c6455b604c449bdbdb3a2b3298a
HugoMoran3 <hugo.moran3@gmail.com>
2021-08-25 17:25:55 +0930

    Reduced projects

--- src/components/card.js
+++ src/components/card.js
@@ -23,26 +23,6 @@ const Card = () => {
                 </div>
                 </div>
             </div>
-            <div class="item">
-                <a href="#0" aria-labelledby="project3"></a>
-                <img src='https://i.imgur.com/DkfiBxR.jpeg' alt=''/>
-                <div class="cardoverlay">
-                <h3 id="project3" aria-hidden="true">Person with a really long name</h3>
-                <div class="cardbody">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-                </div>
-            </div>
-            <div class="item">
-                <a href="#0" aria-labelledby="project4"></a>
-                <img src='https://i.imgur.com/DkfiBxR.jpeg' alt=''/>
-                <div class="cardoverlay">
-                <h3 id="project4" aria-hidden="true">Person with an even longer name that wraps onto multiple lines</h3>
-                <div class="cardbody">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-                </div>
-            </div>
         </div>
     )
 }
